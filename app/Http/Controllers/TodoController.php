f074657f03476269c83b0a1604f35bf1d968634f
anhanh03 <97895905+anhanh03@users.noreply.github.com>
2024-08-21 21:19:57 +0700

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function index()
+{
+    $todos = [];
+
+    if (auth()->check()) {
+        // Lấy id của user hiện tại đang đăng nhập
+        $userId = auth()->user()->id;
+
+        // Lấy danh sách các todo của user hiện tại
+        $todos = Todo::where('user_id', $userId)->get();
+    }
+
+    return view('home', compact('todos'));
+}
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255', // Tiêu đề là bắt buộc và tối đa 255 ký tự
+        ]);
+
+        // Thêm cột 'user_id' vào mảng dữ liệu trước khi tạo mới todo
+        $todoData = [
+            'title' => $request->title,
+            'completed' => false, // Mặc định công việc mới chưa hoàn thành
+            'user_id' => $request->user_id, // Lấy giá trị 'user_id' từ request
+        ];
+
+        Todo::create($todoData);
+
+        return redirect()->route('dashboard')->with('status', 'Todo added successfully!');
+    }
+
+    public function edit($id)
+    {
+        $todo = Todo::findOrFail($id);
+        return view('edit', compact('todo'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        $todo = Todo::findOrFail($id);
+        $todo->update([
+            'title' => $request->title,
+        ]);
+
+        return redirect()->route('dashboard')->with('status', 'Todo updated successfully!');
+    }
+
+    public function destroy($id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+
+        return redirect()->route('dashboard')->with('status', 'Todo deleted successfully!');
+    }
+    public function updateCheck(Request $request, $id)
+    {
+        $request->validate([
+            'completed' => 'required|boolean',
+        ]);
+
+        $todo = Todo::findOrFail($id);
+        $todo->update([
+            'completed' => $request->completed,
+        ]);
+
+        return response()->json(['message' => 'Todo updated successfully!']);
+    }
+
+}
+
